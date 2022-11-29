56fe20a4f094eb46f141fbbd4e66959b1fdc17bc
Bence Hornyák <sir.bence15@gmail.com>
2022-11-29 09:23:48 +0100

    feat(posts/list): initla posts tree with filter

--- projects/posts/src/lib/posts.component.ts
+++ projects/posts/src/lib/posts.component.ts
@@ -1,15 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { changeFilterOption, loadPosts, State } from './+state';
+import { FilterOptions } from './models';
+import * as selectors from './+state';
 
 @Component({
   selector: 'lib-posts',
   template: `
-    <p>
-      posts works!
-    </p>
+    <lib-filter
+      [filters]="filters"
+      [selectedFilter]="selectedFilterOption$ | async"
+      (filterSelectChange)="groupingChange($event)"
+    ></lib-filter>
+    <lib-tree-view
+      [postNodes]="postNodes$ | async"
+      (openNodeClick)="navigateToPostWithId($event)"
+    ></lib-tree-view>
   `,
-  styles: [
-  ]
+  styles: [],
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit {
+  readonly filters = ['author', 'location', 'week'] as FilterOptions[];
+  readonly selectedFilterOption$ = this.store.select(
+    selectors.currentFilterOption
+  );
 
+  readonly postNodes$ = this.store.select(selectors.postNodes);
+
+  constructor(private store: Store<State>) {}
+
+  ngOnInit(): void {
+    this.store.dispatch(loadPosts());
+  }
+
+  groupingChange(filterOption: FilterOptions) {
+    this.store.dispatch(changeFilterOption({ filterOption }));
+  }
+
+  navigateToPostWithId(id: number) {
+    console.log(id);
+  }
 }
