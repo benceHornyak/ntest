cb0ffefb9b9d737f851009257c0d0e499386e5d1
Bence Hornyák <sir.bence15@gmail.com>
2022-11-29 09:21:00 +0100

    feat(ui/tree-view): initial tree-view component

--- projects/ui/src/lib/tree-view/tree-view.component.ts
+++ projects/ui/src/lib/tree-view/tree-view.component.ts
@@ -1,11 +1,39 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NestedTreeControl } from '@angular/cdk/tree';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import { MatTreeNestedDataSource } from '@angular/material/tree';
+import { PostNode } from 'projects/posts/src/public-api';
 
 @Component({
   selector: 'lib-tree-view',
   templateUrl: './tree-view.component.html',
   styleUrls: ['./tree-view.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TreeViewComponent {
+export class TreeViewComponent implements OnChanges {
+  @Input() postNodes: PostNode[] | null = [];
+  @Output() openNodeClick = new EventEmitter<number>();
 
+  treeControl = new NestedTreeControl<PostNode>((node) => node.children);
+  dataSource = new MatTreeNestedDataSource<PostNode>();
+
+  hasChild = (_: number, node: PostNode) =>
+    node.children && node.children.length > 0;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['postNodes'].currentValue) {
+      this.dataSource.data = [...(changes['postNodes'].currentValue ?? [])];
+    }
+  }
+
+  nodeOpenClicked(id: number) {
+    this.openNodeClick.emit(id);
+  }
 }
--- projects/ui/src/lib/ui.module.ts
+++ projects/ui/src/lib/ui.module.ts
@@ -5,10 +5,21 @@ import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { CommonModule } from '@angular/common';
 import { TreeViewComponent } from './tree-view/tree-view.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [FilterComponent, TreeViewComponent],
-  imports: [MatSelectModule, MatFormFieldModule, FormsModule, CommonModule],
+  imports: [
+    MatSelectModule,
+    MatFormFieldModule,
+    FormsModule,
+    CommonModule,
+    MatTreeModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   exports: [FilterComponent, TreeViewComponent],
 })
 export class UiModule {}
