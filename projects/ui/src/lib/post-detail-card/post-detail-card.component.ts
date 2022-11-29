c129b7e2adc72da202e63170540313bcaeda0df8
Bence Hornyák <sir.bence15@gmail.com>
2022-11-29 14:23:44 +0100

    feat(ui/post-detail): basic card with auto save form..

--- projects/ui/src/lib/post-detail-card/post-detail-card.component.ts
+++ projects/ui/src/lib/post-detail-card/post-detail-card.component.ts
@@ -1,11 +1,64 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Post } from 'projects/posts/src/public-api';
+import { combineLatest, map, skip, startWith, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'lib-post-detail-card',
   templateUrl: './post-detail-card.component.html',
   styleUrls: ['./post-detail-card.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PostDetailCardComponent {
+export class PostDetailCardComponent implements OnInit, OnDestroy {
+  @Input() postDetail!: Post;
+  @Output() valueChanges = new EventEmitter<Post>();
+  @Output() backClicked = new EventEmitter<void>();
+  authorCtrl = new FormControl('');
+  locationCtrl = new FormControl('');
 
+  private destroy = new Subject<void>();
+
+  ngOnInit(): void {
+    const { location, author } = this.postDetail;
+
+    this.authorCtrl.patchValue(author);
+    this.locationCtrl.patchValue(location);
+
+    combineLatest({
+      author: this.authorCtrl.valueChanges.pipe(
+        startWith(this.authorCtrl.value),
+        map((v) => v as string)
+      ),
+      location: this.locationCtrl.valueChanges.pipe(
+        startWith(this.locationCtrl.value),
+        map((v) => v as string)
+      ),
+    })
+      .pipe(
+        skip(1),
+        map((newData) => ({
+          ...this.postDetail,
+          ...newData,
+        })),
+        takeUntil(this.destroy)
+      )
+      .subscribe((v) => this.valueChanges.emit(v));
+  }
+
+  goBack() {
+    this.backClicked.emit();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy.next();
+    this.destroy.complete();
+  }
 }
--- projects/ui/src/lib/ui.module.ts
+++ projects/ui/src/lib/ui.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { FilterComponent } from './filter/filter.component';
 import { MatSelectModule } from '@angular/material/select';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
 import { TreeViewComponent } from './tree-view/tree-view.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTreeModule } from '@angular/material/tree';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { PostDetailCardComponent } from './post-detail-card/post-detail-card.component';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [FilterComponent, TreeViewComponent, PostDetailCardComponent],
@@ -16,10 +18,13 @@ import { PostDetailCardComponent } from './post-detail-card/post-detail-card.com
     MatSelectModule,
     MatFormFieldModule,
     FormsModule,
+    ReactiveFormsModule,
     CommonModule,
     MatTreeModule,
     MatIconModule,
     MatButtonModule,
+    MatCardModule,
+    MatInputModule,
   ],
   exports: [FilterComponent, TreeViewComponent, PostDetailCardComponent],
 })
