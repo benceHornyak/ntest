7183c200b8ce0eba517aed52f68774f31b822330
Bence Hornyák <sir.bence15@gmail.com>
2022-11-29 14:25:13 +0100

    feat(posts/detail): provide data, navigate back on back click

--- projects/posts/src/lib/post-detail/post-detail.component.ts
+++ projects/posts/src/lib/post-detail/post-detail.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import * as PostsActions from '../+state';
 import * as PostsSelector from '../+state';
+import { Post } from '../models';
 
 @Component({
   selector: 'lib-post-detail',
@@ -12,10 +13,22 @@ import * as PostsSelector from '../+state';
 })
 export class PostDetailComponent implements OnInit {
   readonly postDetail$ = this.store.select(PostsSelector.getPostDetail);
-  constructor(private route: ActivatedRoute, private store: Store) {}
+  constructor(
+    private route: ActivatedRoute,
+    private store: Store,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     const postId = Number(this.route.snapshot.paramMap.get('id'));
     this.store.dispatch(PostsActions.loadPostDetail({ postId }));
+  }
+
+  navigateBack() {
+    this.router.navigate(['posts']);
+  }
+
+  saveNewPost(newPost: Post) {
+    this.store.dispatch(PostsActions.updatePost({ post: newPost }));
   }
 }
